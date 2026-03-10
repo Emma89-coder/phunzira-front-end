fa2f2f6f4dbd2699acb53035772a769e2024d61c
Emma89 <mchinthae@gmail.com>
2026-03-10 02:55:25 +0200

    Remove backend and IDE files from repository

--- back-end/migrations/20260307115715_initial_schema.sql
+++
@@ -1,8 +0,0 @@
--- Migration: initial_schema
--- Created: 2026-03-07T11:57:15.146Z
-
--- Write your SQL here
--- Example:
--- CREATE TABLE example (id SERIAL PRIMARY KEY);
--- ALTER TABLE users ADD COLUMN phone VARCHAR(20);
-
